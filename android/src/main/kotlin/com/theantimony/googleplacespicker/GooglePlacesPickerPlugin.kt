79ecbf40627274b665812e7b85d363b1cae51103
Elena Porcelli <elena@dedecube.com>
2020-03-08 21:39:22 +0100

    Kotlin bug fix

--- android/src/main/kotlin/com/theantimony/googleplacespicker/GooglePlacesPickerPlugin.kt
+++ android/src/main/kotlin/com/theantimony/googleplacespicker/GooglePlacesPickerPlugin.kt
@@ -198,15 +198,15 @@ class GooglePlacesPickerPlugin() : FlutterPlugin, MethodCallHandler, PluginRegis
             if (addressComponents != null) {
                 var locality: String = ""
                 var country: String = ""
-                for (address in addressComponents) {
-                    if (address.types.contains("locality")) {
-                        val transformed = address.name;
+                addressComponents.forEach {
+                    if (it.types.contains("locality")) {
+                        val transformed = it.name;
                         locality = transformed
-                    } else if (address.types.contains("country")) {
-                        val transformed = address.name;
+                    } else if (it.types.contains("country")) {
+                        val transformed = it.name;
                         country = transformed
-                    } else if (address.types.contains("administrative_area_level_3") && locality == "") {
-                        val transformed = address.name;
+                    } else if (it.types.contains("administrative_area_level_3") && locality == "") {
+                        val transformed = it.name;
                         locality = transformed
                     }
                 }
